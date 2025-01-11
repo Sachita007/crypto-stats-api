fee3d98fd11d93179e38368d82cad6c6d7d7f386
Sachita Nand <shachitanandk@gmail.com>
2025-01-11 07:40:07 +0530

    Minor bug fixes

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -1,10 +1,10 @@
-const { app: supportedCoins } = require('./../config');
+const { app: { supportedCoins } } = require('./../config');
 
 const validateCoin = (req, res, next) => {
     const { coin } = req.query;
     if (!coin || !supportedCoins.includes(coin)) {
         return res.status(400).json({
-            error: 'Invalid coin. Supported coins: ' + config.coinGecko.supportedCoins.join(', '),
+            error: 'Invalid coin. Supported coins: ' + supportedCoins.join(', '),
         });
     }
     next();
